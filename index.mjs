a4764d7f8bb16739a7cb688595d083d86043f88c
saramnim <12hyeji24@gmail.com>
2023-05-04 17:32:08 +0900

    fix: 하지만 더 수정해야함!노드에서는 document가 먹지 않아요!

---
+++ index.mjs
@@ -0,0 +1,21 @@
+var mysql = require("mysql2");
+import { sql } from "./buttonChange";
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "1234",
+  database: "hello_zoo",
+});
+
+con.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
+  // var sql = "create table animal_detail(animal_id int primary key, growth_generation int, birth_date date, growth_weight int, growth_health int, growth_date int);"
+  // var sql = "drop table animal_detail;"
+
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Table created");
+  });
+});
